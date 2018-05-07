7ee042849a8c14d659bee4f89db14a5aae6d24eb
KiHyeonYun <ykh094@naver.com>
2018-05-08 02:10:38 +0900

    메인페이지 tabhost

--- app/src/main/java/com/example/yun/togethertogether/MainActivity.java
+++ app/src/main/java/com/example/yun/togethertogether/MainActivity.java
@@ -1,35 +1,42 @@
 package com.example.yun.togethertogether;
 
+        import android.content.Intent;
         import android.support.v4.content.res.ResourcesCompat;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.view.View;
         import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
-    TabHost tabHost; //탭호스트 객체 생성
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle(" TOTO");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         TabHost tabHost = (TabHost)findViewById(R.id.host);
         tabHost.setup();
 
         TabHost.TabSpec tabSpec = tabHost.newTabSpec("tab01");
-
-        tabSpec.setIndicator(null, ResourcesCompat.getDrawable(getResources(),R.drawable.power,null));
-        tabSpec.setContent(R.id.content01);
+        tabSpec.setIndicator("HOME",null);
+        tabSpec.setContent(R.id.tab1);
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tab02");
-        tabSpec.setIndicator(null, ResourcesCompat.getDrawable(getResources(),R.drawable.power4,null));
-        tabSpec.setContent(R.id.content02);
+        tabSpec.setIndicator("THREE",null);
+        tabSpec.setContent(R.id.tab2);
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tab03");
-        tabSpec.setIndicator(null, ResourcesCompat.getDrawable(getResources(),R.drawable.power3,null));
-        tabSpec.setContent(R.id.content03);
+        tabSpec.setIndicator("FOUR",null);
+        tabSpec.setContent(R.id.tab3);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tab04");
+        tabSpec.setIndicator("더보기",null);
+        tabSpec.setContent(R.id.tab4);
         tabHost.addTab(tabSpec);
     }
+
 }
