a86abf91893daaa7016953fa73ca793138ba2f04
vhok74 <wogh8732@naver.com>
2018-05-12 13:23:42 +0900

    Home-Color is changed with gradiention

--- app/src/main/java/com/example/yun/togethertogether/MainActivity.java
+++ app/src/main/java/com/example/yun/togethertogether/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.yun.togethertogether;
 
         import android.content.Intent;
+        import android.graphics.drawable.AnimationDrawable;
+        import android.graphics.drawable.Drawable;
         import android.support.v4.content.res.ResourcesCompat;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
+        import android.widget.ImageView;
+        import android.widget.LinearLayout;
         import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout myLayout;
+    AnimationDrawable animationDrawable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle(" TOTO");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myLayout=(LinearLayout)findViewById(R.id.myLayout);
+        animationDrawable=(AnimationDrawable)myLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(4500);
+        animationDrawable.setExitFadeDuration(4500);
+        animationDrawable.start();
+
+        Drawable alpha = ((ImageView)findViewById(R.id.love_sentence)).getDrawable();
+        alpha.setAlpha(80);
 
         TabHost tabHost = (TabHost)findViewById(R.id.host);
         tabHost.setup();
